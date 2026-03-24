ff5653c101b7453c9b51a8e3c3028b4b9df0b3ae
viewfc <viewfc118@gmail.com>
2026-03-24 21:35:46 +0700

    Update app.py

--- school_management_system/app.py
+++ school_management_system/app.py
@@ -110,5 +110,7 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    init_db()
+    with app.app_context():
+        init_db()
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
